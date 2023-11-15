e342fdd452d2663d81de9a97e0b6a8963127b27f
Dominik Linkowski <dominiklinq@gmail.com>
2023-11-15 14:10:27 +0100

    remove comment

--- src/error.ts
+++ src/error.ts
@@ -1,17 +1,3 @@
-/***************************************************************
- * Copyright (c) 2023 Reste Dominik Linkowski
- * All rights reserved.
- *
- * This file is protected by copyright law. Any reproduction,
- * modification, distribution, or use in any form requires
- * written permission from the author.
- *
- * Unauthorized use, reproduction, or distribution of this file
- * or any portion of it may result in severe civil and criminal
- * penalties, and will be prosecuted to the maximum extent
- * possible under the law.
- ***************************************************************/
-
 export class ServerError extends Error {
   constructor(
     public readonly message: string,
