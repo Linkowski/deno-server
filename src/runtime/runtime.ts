38f54ec457405b5f8d586d8347ca4aa40ff27112
Dominik Linkowski <dominiklinq@gmail.com>
2024-08-05 22:15:59 +0200

    v10

--- src/runtime/runtime.ts
+++ src/runtime/runtime.ts
@@ -13,12 +13,17 @@ export async function runtime(
   // Load files from the controllers directory
   if (options?.controllers) {
     const controllerPath = new URL(options.controllers, import.meta.url).href;
+    console.log('controllerPath', controllerPath);
     const files = Array.from(Deno.readDirSync(controllerPath));
 
     for (const file of files) {
       // and ignore files with .test.ts extension
       if (file.isFile && file.name.endsWith('.ts') && !file.name.endsWith('.test.ts')) {
-        await import(`${controllerPath}/${file.name}`);
+        const filePath = new URL(file.name, controllerPath);
+
+        console.log('filePath', filePath);
+
+        await import(filePath.href);
       }
     }
   }
